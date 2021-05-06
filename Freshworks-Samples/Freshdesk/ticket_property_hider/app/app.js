ca9216c38306293dda048cdbee678dea3dff5f9c
BT <77515518+bjt54@users.noreply.github.com>
2021-05-06 10:20:00 -0700

    Update app.js
    Change disable property to editor window "to" field

--- Freshworks-Samples/Freshdesk/ticket_property_hider/app/app.js
+++ Freshworks-Samples/Freshdesk/ticket_property_hider/app/app.js
@@ -5,7 +5,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // App activate callback
     client.events.on('app.activated', function() {
       // Disable Ticket Priority
-      client.interface.trigger('disable', { id: 'priority'} );
+    client.interface.trigger("disable", {id: "to"})
+        .then(function(data) {
+        // data - success message
+        }).catch(function(error) {
+        // error - error object
     });
   });
 });

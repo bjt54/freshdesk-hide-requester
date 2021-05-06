61cf6f1cc9e4480604ec181f2cc873ac51067e1d
BT <77515518+bjt54@users.noreply.github.com>
2021-05-06 11:40:51 -0700

    fix js errors

--- Freshworks-Samples/Freshdesk/ticket_property_hider/app/app.js
+++ Freshworks-Samples/Freshdesk/ticket_property_hider/app/app.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize channel
-  app.initialized().then(function(_client) {
-    var client = _client;
+    app.initialized().then(function (_client) {
+        var client = _client;
     // App activate callback
-    client.events.on('app.activated', function() {
+        client.events.on('app.activated', function () {
       // Disable Ticket Priority
-    client.interface.trigger("disable", {id: "to"})
-        .then(function(data) {
-        // data - success message
-        }).catch(function(error) {
-        // error - error object
+            client.interface.trigger('hide', { id: 'to'} );
     });
   });
 });
